bf274cd5493ea4e327b82a33c0ecf9bc6e96fdee
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-06-26 00:42:49 +0700

    feat: added uploaded_at property inside EMR.

--- traits/electronic-medical-record/src/lib.rs
+++ traits/electronic-medical-record/src/lib.rs
@@ -1,15 +1,18 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use frame_system::Config;
+use pallet_timestamp::Config as TimeConfig;
 //use sp_std::prelude::*;
 
-pub trait ElectronicMedicalRecordInfo<T: Config> {
+pub trait ElectronicMedicalRecordInfo<T: Config + TimeConfig> {
     fn get_id(&self) -> &T::Hash;
+    fn get_uploaded_at(&self) -> &T::Moment;
     fn get_owner_id(&self) -> &T::AccountId;
 }
 
-pub trait ElectronicMedicalRecordInfosProvider<T: Config> {
+pub trait ElectronicMedicalRecordInfosProvider<T: Config + TimeConfig> {
     type Error;
+    type Moment;
     type ElectronicMedicalRecordInfo: ElectronicMedicalRecordInfo<T> + sp_std::fmt::Debug;
 
     fn remove_electronic_medical_record_info(owner_id: &T::AccountId, electronic_medical_record_info_id: &T::Hash) -> Result<Self::ElectronicMedicalRecordInfo, Self::Error>;
