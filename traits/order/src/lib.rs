3d78f626f110cb1f96f8926de47be68ac9c5b586
Kevin Janada <kevin.janada@gmail.com>
2021-07-26 11:46:27 +0700

    feat: Update order status to failed on dna sample rejected
    - Add enum OrderStatus::Failed
    - Create trait OrderStatusUpdater
    - implement OrderStatusUpdater.update_order_failed_status
    - Update types.json

--- traits/order/src/lib.rs
+++ traits/order/src/lib.rs
@@ -5,3 +5,7 @@ use frame_system::Config;
 pub trait OrderEventEmitter<T: Config> {
     fn emit_event_order_failed(order_id: &T::Hash) -> ();
 }
+
+pub trait OrderStatusUpdater<T: Config> {
+    fn update_status_failed(order_id: &T::Hash) -> ();
+}
