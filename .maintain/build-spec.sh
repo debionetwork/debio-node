92edbdea509add20ad3fb4cc839c370fa10a8122
Agustinus Theodorus <agustinustheoo@gmail.com>
2021-10-21 09:36:37 +0700

    fix: build and docker scripts.

--- .maintain/build-spec.sh
+++ .maintain/build-spec.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cargo build --release
-./target/release/debio-node build-spec --disable-default-bootnode --chain dev> debioSpec.json 
-sha256sum debioSpec.json> SHA256SUMS
+./target/release/debio-node build-spec --disable-default-bootnode --chain debio-genesis > ./node/res/main/debioSpec.json 
+sha256sum ./node/res/main/debioSpec.json> ./node/res/main/SHA256SUMS
