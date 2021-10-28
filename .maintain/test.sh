721d26342eff733d21419a42fadc504b74d95cef
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-10-28 11:17:48 +0800

    added new scripts for test bench helper

---
+++ .maintain/test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+pushd .
+
+# The following line ensure we run from the project root
+PROJECT_ROOT=`git rev-parse --show-toplevel`
+cd $PROJECT_ROOT
+
+echo "Check Code"
+cargo +nightly-2021-06-29 check --all
+echo "Test Code"
+cargo +nightly-2021-06-29 test --all
+echo "Check Lint"
+cargo +nightly-2021-06-29 clippy --all -- -D warnings
+echo "Check Format"
+cargo +nightly-2021-06-29 fmt --all -- --check
+
+popd
