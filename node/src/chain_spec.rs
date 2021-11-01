796a0abb84a5d6540ffcf372c958a2626ba7dc72
Agustinus Theodorus <agustinustheoo@gmail.com>
2021-11-01 23:26:12 +0700

    fix: development chainspec.

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -121,12 +121,22 @@ pub fn testnet_config() -> Result<ChainSpec, String> {
 				// WASM Binary
 				wasm_binary,
 				// Sudo account
-				// 5FR73HBVwSpPjnPsBZPDVyuHQS1KE8jvSL3pSud6F6HZcuBA
-				hex!["9443a63297b9f5b4e2569ee17225011db11a537066bce62d018acbcfda88f947"].into(),
+				// 5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+				hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
 				// Initial PoA authorities
-				vec![],
+				vec![
+					authority_keys_from_seed("Alice"),
+					authority_keys_from_seed("Bob"),
+				],
 				// Pre-funded accounts
-				vec![],
+				vec![
+					// Sudo     5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+					hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
+					// Faucet   5HbNav6B8wUj8F9jRCVEcL6a576iHP8HJhfSfZM7fEHnRs2X
+					hex!["f490e69c55aa14d06bb5d62d12b81db20f3c125d6ea5d1cfddfcf98767272e6b"].into(),
+					// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+					hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
+				],
 				// Appchain config
 				appchain_config(
 					// Appchain Id
@@ -141,14 +151,14 @@ pub fn testnet_config() -> Result<ChainSpec, String> {
 					1024,
 				),
 				// Orders Pallet admin key
-				// 5FpcRYvUMB3bNRdbj5YDwKeGHKVeWmdjnzY45RdTJSoSGcKN
-				hex!["a63135764844b7b889f0447cc5127c4aa1b78fb998878549bf66ed7b0ee49753"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Rewarders Pallet admin key
-				// 5Da5bmXVafegKWxuhx7bNDwLi3QAAwJDfDGrtd751Z2pKQPX
-				hex!["42a38b41fe71d9d08ab8d560dd0268cdcc962dadfc3b0fccba88976316f3304a"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Lab Verifier Pallet admin key
-				// 5CLvbDokzbFdTpbmsu1TiY81Hgj1U5HGpimdV6pvsVU9ngGa
-				hex!["0c5ecab4fc8add2ad833f9d56082385771b141655a95d9383b39002483644f10"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 			)
 		},
 		// Bootnodes
@@ -179,8 +189,8 @@ pub fn staging_testnet_config() -> Result<ChainSpec, String> {
 				// WASM Binary
 				wasm_binary,
 				// Sudo account
-				// 5CB5udaxY6zFqApVHWPQTGTW5FszotkXKAUD48fvi5Y7FSR2
-				hex!["04ddb3f730857ed801327da2242dff4d4d85e25b33c43db6f328d55904247f40"].into(),
+				// 5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+				hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
 				// Initial PoA authorities
 				vec![
 					(
@@ -214,14 +224,12 @@ pub fn staging_testnet_config() -> Result<ChainSpec, String> {
 				],
 				// Pre-funded accounts
 				vec![
-					// 5CB5udaxY6zFqApVHWPQTGTW5FszotkXKAUD48fvi5Y7FSR2
-					hex!["04ddb3f730857ed801327da2242dff4d4d85e25b33c43db6f328d55904247f40"].into(),
-					// 5ELYNFhFz9tauMxfjgTGhd6sRbnndddEXqh3UxWsPi6Rjajg
-					hex!["648c728f7fcf0ae26a44410cf0ba4ea15b27b3169a4f809a14097680b8d0bc53"].into(),
-					// 5CB5udaxY6zFqApVHWPQTGTW5FszotkXKAUD48fvi5Y7FSR2
-					hex!["04ddb3f730857ed801327da2242dff4d4d85e25b33c43db6f328d55904247f40"].into(),
-					// 5EySyU6ufAU485FWh5r44pSXxJWUc1dbzRX6t1K36Qj6syxg
-					hex!["80b25f9b8d044d940be74689485c235c9051e87ea6ee1f3300107a43a02ed150"].into(),
+					// Sudo     5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+					hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
+					// Faucet   5HbNav6B8wUj8F9jRCVEcL6a576iHP8HJhfSfZM7fEHnRs2X
+					hex!["f490e69c55aa14d06bb5d62d12b81db20f3c125d6ea5d1cfddfcf98767272e6b"].into(),
+					// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+					hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				],
 				// Appchain config
 				appchain_config(
@@ -237,14 +245,14 @@ pub fn staging_testnet_config() -> Result<ChainSpec, String> {
 					1024,
 				),
 				// Orders Pallet admin key
-				// 5ELYNFhFz9tauMxfjgTGhd6sRbnndddEXqh3UxWsPi6Rjajg
-				hex!["648c728f7fcf0ae26a44410cf0ba4ea15b27b3169a4f809a14097680b8d0bc53"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Rewarders Pallet admin key
-				// 5CB5udaxY6zFqApVHWPQTGTW5FszotkXKAUD48fvi5Y7FSR2
-				hex!["04ddb3f730857ed801327da2242dff4d4d85e25b33c43db6f328d55904247f40"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Lab Verifier Pallet admin key
-				// 5EySyU6ufAU485FWh5r44pSXxJWUc1dbzRX6t1K36Qj6syxg
-				hex!["80b25f9b8d044d940be74689485c235c9051e87ea6ee1f3300107a43a02ed150"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 			)
 		},
 		// Bootnodes
@@ -275,8 +283,8 @@ pub fn development_testnet_config() -> Result<ChainSpec, String> {
 				// WASM Binary
 				wasm_binary,
 				// Sudo account
-				// 5G3nLeySH5sFzD9WPKt2kB3KNVnazsZykaFfotouvjf1RZWY
-				hex!["b03cc727c3c98eab988e5acfa815f6e6ed1939060471adaa78d2e39bbb1fc50b"].into(),
+				// 5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+				hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
 				// Initial PoA authorities
 				vec![
 					(
@@ -310,14 +318,12 @@ pub fn development_testnet_config() -> Result<ChainSpec, String> {
 				],
 				// Pre-funded accounts
 				vec![
-					// 5G3nLeySH5sFzD9WPKt2kB3KNVnazsZykaFfotouvjf1RZWY
-					hex!["b03cc727c3c98eab988e5acfa815f6e6ed1939060471adaa78d2e39bbb1fc50b"].into(),
-					// C8KpmHUFT7HJbNLv74cXrtT1w9LF1W3WduN8nVGQUySSJTF
-					hex!["02c2cffef38fbf56b32d6a49eeeecc0e3345a1e0549cd8817d52f6cf2e414152"].into(),
-					// 5Hg7C14cu9W4ihHkTkJz8Q3fTHquhNN2N6a7v4dhueNxxbup
-					hex!["f82d4dac27cf82548f8714e16c5220e20585eb08716194413489544392130877"].into(),
-					// 5H6CqoMzgVeoDNJXMC6C5nBnJDjzGyLaJv36inixUGRxD2Uv
-					hex!["de51fc26417995f8cad8e66f805d1446dd7b987d7e60b822bfbe9988832e2708"].into(),
+					// Sudo     5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+					hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
+					// Faucet   5HbNav6B8wUj8F9jRCVEcL6a576iHP8HJhfSfZM7fEHnRs2X
+					hex!["f490e69c55aa14d06bb5d62d12b81db20f3c125d6ea5d1cfddfcf98767272e6b"].into(),
+					// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+					hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				],
 				// Appchain config
 				appchain_config(
@@ -333,14 +339,14 @@ pub fn development_testnet_config() -> Result<ChainSpec, String> {
 					1024,
 				),
 				// Orders Pallet admin key
-				// C8KpmHUFT7HJbNLv74cXrtT1w9LF1W3WduN8nVGQUySSJTF
-				hex!["02c2cffef38fbf56b32d6a49eeeecc0e3345a1e0549cd8817d52f6cf2e414152"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Rewarders Pallet admin key
-				// 5Hg7C14cu9W4ihHkTkJz8Q3fTHquhNN2N6a7v4dhueNxxbup
-				hex!["f82d4dac27cf82548f8714e16c5220e20585eb08716194413489544392130877"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Lab Verifier Pallet admin key
-				// 5H6CqoMzgVeoDNJXMC6C5nBnJDjzGyLaJv36inixUGRxD2Uv
-				hex!["de51fc26417995f8cad8e66f805d1446dd7b987d7e60b822bfbe9988832e2708"].into(),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 			)
 		},
 		// Bootnodes
@@ -371,17 +377,21 @@ pub fn local_config() -> Result<ChainSpec, String> {
 				// WASM Binary
 				wasm_binary,
 				// Sudo account
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// 5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+				hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
 				// Initial PoA authorities
-				vec![authority_keys_from_seed("Alice"), authority_keys_from_seed("Bob")],
+				vec![
+					authority_keys_from_seed("Alice"),
+					authority_keys_from_seed("Bob"),
+				],
 				// Pre-funded accounts
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie"),
-					get_account_id_from_seed::<sr25519::Public>("Dave"),
-					get_account_id_from_seed::<sr25519::Public>("Eve"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					// Sudo     5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+					hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
+					// Faucet   5HbNav6B8wUj8F9jRCVEcL6a576iHP8HJhfSfZM7fEHnRs2X
+					hex!["f490e69c55aa14d06bb5d62d12b81db20f3c125d6ea5d1cfddfcf98767272e6b"].into(),
+					// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+					hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				],
 				// Appchain config
 				appchain_config(
@@ -397,14 +407,14 @@ pub fn local_config() -> Result<ChainSpec, String> {
 					1024,
 				),
 				// Orders Pallet admin key
-				// 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Rewarders Pallet admin key
-				// 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Lab Verifier Pallet admin key
-				// 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 			)
 		},
 		// Bootnodes
@@ -435,17 +445,18 @@ pub fn development_config() -> Result<ChainSpec, String> {
 				// WASM Binary
 				wasm_binary,
 				// Sudo account
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// 5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+				hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
 				// Initial PoA authorities
 				vec![authority_keys_from_seed("Alice")],
 				// Pre-funded accounts
 				vec![
-					get_account_id_from_seed::<sr25519::Public>("Alice"),
-					get_account_id_from_seed::<sr25519::Public>("Bob"),
-					get_account_id_from_seed::<sr25519::Public>("Charlie"),
-					get_account_id_from_seed::<sr25519::Public>("Dave"),
-					get_account_id_from_seed::<sr25519::Public>("Eve"),
-					get_account_id_from_seed::<sr25519::Public>("Ferdie"),
+					// Sudo     5EpzDTRWDoVTnE31ybM2tse77CkZyG2eKC58Z3gbALHphHN6
+					hex!["7a3e54fe532670c009cc839a7a9b8578239d08ed5234909d991da8ba39f45346"].into(),
+					// Faucet   5HbNav6B8wUj8F9jRCVEcL6a576iHP8HJhfSfZM7fEHnRs2X
+					hex!["f490e69c55aa14d06bb5d62d12b81db20f3c125d6ea5d1cfddfcf98767272e6b"].into(),
+					// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+					hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				],
 				// Appchain config
 				appchain_config(
@@ -461,14 +472,14 @@ pub fn development_config() -> Result<ChainSpec, String> {
 					1024,
 				),
 				// Orders Pallet admin key
-				// 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Rewarders Pallet admin key
-				// 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 				// Lab Verifier Pallet admin key
-				// 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
+				// API Server   5GRjDZsTCatwWfNosGF8QRAPR1zYPJ7jJppt224tjE7x8cSx
+				hex!["c0f9aaa3ce6b6c57eadc5fef443aaf8152fa8e49a8fc684ecc47c3304fdf3c0c"].into(),
 			)
 		},
 		// Bootnodes
