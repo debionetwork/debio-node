80d8cae50f5a1259c585d0c43f5518e15a5d670c
Agustinus Theodorus <agustinustheoo@gmail.com>
2021-11-18 11:53:24 +0700

    fix: region and city code.

--- primitives/area-code/src/lib.rs
+++ primitives/area-code/src/lib.rs
@@ -34,14 +34,11 @@ impl AreaCode for CountryCode {
 
 // RegionCode -> YY
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq, TypeInfo)]
-pub struct RegionCode([u8; 2]); 
+pub struct RegionCode(Vec<u8>); 
 
 impl RegionCode {
     pub fn from_vec(_vec_id: Vec<u8>) -> Self {
-        let _array = _vec_id.try_into()
-            .unwrap_or_else(|_vec_id: Vec<u8>| panic!("Expected a Vec of length {} but it was {}", 2, _vec_id.len()));
-        
-        Self(_array)
+        Self(_vec_id)
     }
 }
 
@@ -53,14 +50,11 @@ impl AreaCode for RegionCode {
 
 // CityCode -> ZZZZ
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq, TypeInfo)]
-pub struct CityCode([u8; 4]); // city_code -> ZZZZ
+pub struct CityCode(Vec<u8>); // city_code -> ZZZZ
 
 impl CityCode {
     pub fn from_vec(_vec_id: Vec<u8>) -> Self {
-        let _array = _vec_id.try_into()
-            .unwrap_or_else(|_vec_id: Vec<u8>| panic!("Expected a Vec of length {} but it was {}", 4, _vec_id.len()));
-        
-        Self(_array)
+        Self(_vec_id)
     }
 }
 
@@ -72,14 +66,33 @@ impl AreaCode for CityCode {
 
 // CountryCode-RegionCode -> XX-YY
 #[derive(Encode, Decode, Clone, Default, RuntimeDebug, PartialEq, Eq, TypeInfo)]
-pub struct CountryRegionCode([u8; 5]); 
+pub struct CountryRegionCode(Vec<u8>); 
 
 impl CountryRegionCode {
     pub fn from_vec(_vec_id: Vec<u8>) -> Self {
-        let _array = _vec_id.try_into()
-            .unwrap_or_else(|_vec_id: Vec<u8>| panic!("Expected a Vec of length {} but it was {}", 5, _vec_id.len()));
-        
-        Self(_array)
+        Self(_vec_id)
+    }
+    
+    /// Concatenate CountryCode with RegionCode with a '-'
+    pub fn build_country_region_code(country_code: &CountryCode, region_code: &RegionCode) -> Self {
+        // container
+        let mut country_region_code = Vec::new();
+        let mut country_code = country_code.clone()
+            .to_vec();
+            
+        // dash character as u8
+        let mut dash = ['-'].iter()
+            .map(|c| *c as u8)
+            .collect::<Vec<u8>>();
+
+        let mut region_code = region_code.clone()
+            .to_vec();
+
+        country_region_code.append(&mut country_code);
+        country_region_code.append(&mut dash);
+        country_region_code.append(&mut region_code);
+
+        CountryRegionCode::from_vec(country_region_code)
     }
 }
 
