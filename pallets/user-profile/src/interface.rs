7613072c1b1c4cd9a81dcab55419f10eab238308
abdulhakim2902 <hakim@blocksphere.id>
2021-11-21 21:44:14 +0800

    fixed lint and format user profile

--- pallets/user-profile/src/interface.rs
+++ pallets/user-profile/src/interface.rs
@@ -1,8 +1,6 @@
-//use sp_std::prelude::*;
-
 pub trait UserProfileInterface<T: frame_system::Config, EthAddress> {
-    fn get_eth_address_by_account_id(account_id: &T::AccountId) -> Option<EthAddress>;
-    fn set_eth_address_by_account_id(account_id: &T::AccountId, eth_address: &EthAddress) -> ();
+	fn get_eth_address_by_account_id(account_id: &T::AccountId) -> Option<EthAddress>;
+	fn set_eth_address_by_account_id(account_id: &T::AccountId, eth_address: &EthAddress);
 
-    fn get_account_id_by_eth_address(eth_address: &EthAddress) -> Option<T::AccountId>;
+	fn get_account_id_by_eth_address(eth_address: &EthAddress) -> Option<T::AccountId>;
 }
