ea136f5fd700354cfac5398a45f8857bbe69bda4
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-11-24 11:31:02 +0800

    chore: improve benchmark script

---
+++ .maintain/benchmark-default.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+for entry in `ls pallets`; do
+  pushd .
+
+  # The following line ensure we run from the project root
+  PROJECT_ROOT=`git rev-parse --show-toplevel`
+  cd $PROJECT_ROOT
+
+  PALLET=$entry
+
+  ./target/release/debio benchmark \
+    --chain=dev \
+    --execution=wasm \
+    --wasm-execution=compiled \
+    --pallet="$PALLET" \
+    --extrinsic="*" \
+    --steps=20 \
+    --repeat=10 \
+    --heap-pages=4096 \
+    --raw \
+    --output="./pallets/${PALLET}/src/weights.rs" \
+    --template="./.maintain/pallet-weight-template.hbs"
+
+  popd
+done
--- .maintain/benchmark.sh
+++ .maintain/benchmark.sh
@@ -1,26 +1,47 @@
 #!/usr/bin/env bash
 
-for entry in `ls pallets`; do
-  pushd .
+set -e
 
-  # The following line ensure we run from the project root
-  PROJECT_ROOT=`git rev-parse --show-toplevel`
-  cd $PROJECT_ROOT
+# if [ "$#" -ne 1 ]; then
+# 	echo "Please provide pallet name"
+# 	exit 1
+# fi
 
-  PALLET=$entry
+pushd .
 
-  ./target/release/debio benchmark \
-    --chain=dev \
-    --execution=wasm \
-    --wasm-execution=compiled \
-    --pallet="$PALLET" \
-    --extrinsic="*" \
-    --steps=20 \
-    --repeat=10 \
-    --heap-pages=4096 \
-    --raw \
-    --output="./pallets/${PALLET}/src/weights.rs" \
-    --template="./.maintain/pallet-weight-template.hbs"
+# The following line ensure we run from the project root
+PROJECT_ROOT=`git rev-parse --show-toplevel`
+cd $PROJECT_ROOT
 
-  popd
-done
+# PALLET=$1
+
+# ./target/release/debio benchmark \
+#   --chain=dev \
+#   --execution=wasm \
+#   --wasm-execution=compiled \
+#   --pallet="$PALLET" \
+#   --extrinsic="*" \
+#   --steps=20 \
+#   --repeat=10 \
+#   --heap-pages=4096 \
+#   --raw \
+#   --output="./runtime/src/weights/${PALLET/-/_}.rs"
+
+# shellcheck disable=SC2086
+./target/release/debio benchmark \
+    --chain dev \
+    --list |\
+  tail -n+2 |\
+  cut -d',' -f1 |\
+  uniq | \
+  grep -v frame_system > pallets_list
+
+# For each pallet found in the previous command, run benches on each function
+while read -r line; do
+  pallet="$(echo "$line" | cut -d' ' -f1)";
+  echo "Pallet: $pallet";
+# shellcheck disable=SC2086
+done < pallets_list
+rm pallets_list
+
+popd
