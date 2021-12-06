e7c8961dc7910bbfd6f639b6ab11dc5ef1fbd41d
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2021-12-06 10:51:14 +0800

    chore: fix rewards transfer

--- pallets/rewards/src/benchmarking.rs
+++ pallets/rewards/src/benchmarking.rs
@@ -4,7 +4,6 @@ use crate::{
 	Pallet as Rewards,
 	RewarderKey
 };
-use pallet_sudo::Pallet as Sudo;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite};
 use frame_system::RawOrigin;
 use frame_support::sp_runtime::traits::Saturating;
@@ -17,14 +16,6 @@ benchmarks! {
 	}: reward_funds(
         RawOrigin::Signed(caller.clone()),
         _id,
-        value
-    )
-
-	add_total_reward_balance {
-		let caller: T::AccountId = Sudo::<T>::key();
-		let value = T::Currency::minimum_balance().saturating_mul(100u32.into());
-	}: add_total_reward_balance(
-        RawOrigin::Signed(caller.clone()),
         value
     )
 }
--- pallets/rewards/src/weights.rs
+++ pallets/rewards/src/weights.rs
@@ -30,7 +30,6 @@ use sp_std::marker::PhantomData;
 /// Weight functions needed for rewards.
 pub trait WeightInfo {
 	fn reward_funds() -> Weight;
-	fn add_total_reward_balance() -> Weight;
 }
 
 /// Weights for rewards using the Substrate node and recommended hardware.
@@ -38,14 +37,8 @@ pub struct SubstrateWeight<T>(PhantomData<T>);
 impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 	// Storage: Rewards RewarderKey (r:1 w:0)
 	fn reward_funds() -> Weight {
-		(65_395_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(3 as Weight))
-			.saturating_add(T::DbWeight::get().writes(1 as Weight))
-	}
-	// Storage: Rewards RewarderKey (r:1 w:0)
-	fn add_total_reward_balance() -> Weight {
-		(64_022_000 as Weight)
-			.saturating_add(T::DbWeight::get().reads(2 as Weight))
+		(23_135_000 as Weight)
+			.saturating_add(T::DbWeight::get().reads(1 as Weight))
 			.saturating_add(T::DbWeight::get().writes(1 as Weight))
 	}
 }
@@ -54,14 +47,8 @@ impl<T: frame_system::Config> WeightInfo for SubstrateWeight<T> {
 impl WeightInfo for () {
 	// Storage: Rewards RewarderKey (r:1 w:0)
 	fn reward_funds() -> Weight {
-		(65_395_000 as Weight)
-			.saturating_add(RocksDbWeight::get().reads(3 as Weight))
-			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
-	}
-	// Storage: Rewards RewarderKey (r:1 w:0)
-	fn add_total_reward_balance() -> Weight {
-		(64_022_000 as Weight)
-			.saturating_add(RocksDbWeight::get().reads(2 as Weight))
+		(23_135_000 as Weight)
+			.saturating_add(RocksDbWeight::get().reads(1 as Weight))
 			.saturating_add(RocksDbWeight::get().writes(1 as Weight))
 	}
 }
