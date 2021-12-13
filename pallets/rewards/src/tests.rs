1469adcb521eaae5451e306d70c17f59b6339c97
Agustinus Theodorus <agustinustheoo@gmail.com>
2021-12-14 06:38:14 +0700

    tests: assert RewardFunds event.

--- pallets/rewards/src/tests.rs
+++ pallets/rewards/src/tests.rs
@@ -5,6 +5,8 @@ use frame_support::{assert_noop, assert_ok};
 #[test]
 fn reward_funds_works() {
 	ExternalityBuilder::build().execute_with(|| {
+		System::set_block_number(1);
+
 		assert_eq!(RewarderKey::<Test>::put(1), ());
 
 		assert_ok!(Balances::set_balance(RawOrigin::Root.into(), PalletAccount::<Test>::get(), 100, 0));
@@ -16,6 +18,12 @@ fn reward_funds_works() {
 			)
 		);
 
+		System::assert_last_event(Event::Rewards(crate::Event::RewardFunds(
+			2, 
+			10, 
+			1
+		)));
+
 		assert_eq!(Balances::free_balance(PalletAccount::<Test>::get()), 90);
 	})
 }
@@ -23,6 +31,8 @@ fn reward_funds_works() {
 #[test]
 fn reward_funds_bad_signature() {
 	ExternalityBuilder::build().execute_with(|| {
+		System::set_block_number(1);
+
 		assert_eq!(RewarderKey::<Test>::put(1), ());
 
 		assert_ok!(Balances::set_balance(RawOrigin::Root.into(), PalletAccount::<Test>::get(), 1, 0));
@@ -40,6 +50,8 @@ fn reward_funds_bad_signature() {
 #[test]
 fn cant_reward_funds_when_not_admin() {
 	ExternalityBuilder::build().execute_with(|| {
+		System::set_block_number(1);
+
 		assert_eq!(RewarderKey::<Test>::put(1), ());
 
 		assert_noop!(
