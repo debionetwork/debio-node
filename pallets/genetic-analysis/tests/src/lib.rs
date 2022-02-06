f5b648a4085304c9919b500d8a574da58dc9f61c
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-02-07 02:01:56 +0700

    tests: accept genetic analysis unit test.

--- pallets/genetic-analysis/tests/src/lib.rs
+++ pallets/genetic-analysis/tests/src/lib.rs
@@ -225,6 +225,65 @@ mod tests {
 	}
 	
 	#[test]
+	fn accept_genetic_analysis_works() {
+		<ExternalityBuilder>::default().existential_deposit(1).build().execute_with(|| {
+			assert_ok!(GeneticAnalysts::register_genetic_analyst(
+				Origin::signed(1),
+				GeneticAnalystInfo {
+					first_name: "First Name".as_bytes().to_vec(),
+					last_name: "Last Name".as_bytes().to_vec(),
+					gender: "Gender".as_bytes().to_vec(),
+					date_of_birth: 0,
+					email: "Email".as_bytes().to_vec(),
+					phone_number: "+6893026516".as_bytes().to_vec(),
+					specialization: "DeBio Genetic Analyst".as_bytes().to_vec(),
+					stake_amount: 100,
+					stake_status: StakeStatus::default(),
+				}
+			));
+	
+			assert_ok!(UserProfile::set_eth_address(Origin::signed(1), EthereumAddress([b'X'; 20])));
+	
+			assert_ok!(GeneticAnalystServices::create_genetic_analyst_service(
+				Origin::signed(1),
+				GeneticAnalystServiceInfo {
+					name: "DeBio Genetic Analyst Service name".as_bytes().to_vec(),
+					prices_by_currency: vec![
+						PriceByCurrency::default()
+					],
+					expected_duration: ExpectedDuration::default(),
+					description: "DeBio Genetic Analyst Service description".as_bytes().to_vec(),
+					test_result_sample: "DeBio Genetic Analyst Service test_result_sample".as_bytes().to_vec(),
+				}
+			));
+
+			let _genetic_analyst = GeneticAnalysts::genetic_analyst_by_account_id(1).unwrap();
+	
+			assert_ok!(GeneticAnalysisOrders::create_genetic_analysis_order(
+				Origin::signed(2),
+				_genetic_analyst.services[0],
+				0,
+				Keccak256::hash("0xhJ7TRe456FADD2726A132ABJK5RCc9E6fC5869F4".as_bytes())
+			));
+	
+			let _genetic_analysis = GeneticAnalysis::genetic_analysis_by_genetic_analyst_id(1).unwrap();
+	
+			assert_ok!(
+				GeneticAnalysis::process_genetic_analysis(
+					Origin::signed(1),
+					_genetic_analysis[0].clone(),
+					GeneticAnalysisStatus::InProgress
+				)
+			);
+
+			let _genetic_analysis_info = GeneticAnalysis::genetic_analysis_by_genetic_analysis_tracking_id(_genetic_analysis[0].clone()).unwrap();
+
+			assert_eq!(_genetic_analysis_info.get_genetic_analysis_tracking_id(), &_genetic_analysis[0]);
+			assert_eq!(_genetic_analysis_info.status, GeneticAnalysisStatus::InProgress);
+		})
+	}
+	
+	#[test]
 	fn cannot_process_genetic_analysis_works_not_found() {
 		<ExternalityBuilder>::default().existential_deposit(1).build().execute_with(|| {
 			assert_noop!(
