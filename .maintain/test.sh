04c88cb90747f2332fcf775a8606c2a41a787073
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-02-07 20:22:40 +0700

    fix: test.sh

--- .maintain/test.sh
+++ .maintain/test.sh
@@ -10,8 +10,24 @@ cd $PROJECT_ROOT
 
 echo "Check Code"
 cargo +nightly check --all
-echo "Test Code"
-cargo +nightly test --all
+echo "Start Testing Code"
+for entry in `ls pallets`; do
+  pushd .
+
+  # The following line ensure we run from the project root
+  PROJECT_ROOT=`git rev-parse --show-toplevel`
+  cd $PROJECT_ROOT
+  cd "pallets/${entry}"
+
+  if [ -d "tests" ]; then
+    # Take action if "tests" exists. #
+    cd "tests"
+  fi
+
+  cargo +nightly test
+
+  popd
+done
 echo "Check Lint"
 cargo +nightly clippy --all -- -D warnings
 echo "Check Format"
