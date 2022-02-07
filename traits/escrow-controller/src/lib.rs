fba1a96870b7125b04873ae5418968faadbdbb6a
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-02-08 00:02:56 +0700

    fix: remove escrow controller.

--- traits/escrow-controller/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![cfg_attr(not(feature = "std"), no_std)]
-
-use frame_system::Trait;
-
-pub trait EscrowController<T: Trait> {
-    // Callback to the escrow's controller
-    fn on_escrow_paid(controller_id: &T::Hash) -> ();
-}
