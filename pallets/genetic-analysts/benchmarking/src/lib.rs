1738970922c90c900e2ccffa841081c2946abe29
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-13 15:25:24 +0700

    tests: update genetic analysts benchmark

--- pallets/genetic-analysts/benchmarking/src/lib.rs
+++ pallets/genetic-analysts/benchmarking/src/lib.rs
@@ -195,6 +195,35 @@ benchmarks! {
 		RawOrigin::Signed(caller)
 	)
 
+	retrieve_unstake_amount {
+		let caller_admin: T::AccountId = GeneticAnalystVerifierKey::<T>::get();
+		let caller: T::AccountId = whitelisted_caller();
+		let caller_origin = <T as frame_system::Config>::Origin::from(RawOrigin::Signed(caller.clone()));
+
+		let _ = <T as genetic_analysts::Config>::Currency::deposit_creating(&caller, 60000000000000000000000u128.saturated_into());
+
+		let old_genetic_analyst = GeneticAnalystInfo {
+			box_public_key: T::Hashing::hash("0xDb9Af2d1f3ADD2726A132AA7A65Cc9E6fC5761C3".as_bytes()),
+			first_name: "First Name".as_bytes().to_vec(),
+			last_name: "Last Name".as_bytes().to_vec(),
+			gender: "Gender".as_bytes().to_vec(),
+			date_of_birth: <T as pallet_timestamp::pallet::Config>::Moment::default(),
+			email: "Email".as_bytes().to_vec(),
+			phone_number: "+6893026516".as_bytes().to_vec(),
+			specialization: "DeBio Genetic Analyst".as_bytes().to_vec(),
+			profile_link: "DeBio Genetic Analyst profile_link".as_bytes().to_vec(),
+			profile_image: Some("DeBio Genetic Analyst profile_image".as_bytes().to_vec()),
+		};
+		let _ = GeneticAnalysts::<T>::register_genetic_analyst(caller_origin.clone(), old_genetic_analyst);
+
+		let _ = GeneticAnalysts::<T>::stake_genetic_analyst(caller_origin.clone());
+
+		let _ = GeneticAnalysts::<T>::unstake_genetic_analyst(caller_origin);
+	}: retrieve_unstake_amount(
+		RawOrigin::Signed(caller_admin),
+		caller
+	)
+
 	update_minimum_stake_amount {
 		let caller: T::AccountId = GeneticAnalystVerifierKey::<T>::get();
 	}: update_minimum_stake_amount(
