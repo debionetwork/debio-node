3fe07860c69bcc7f1ecaee229c29bb106f70fa0d
Agustinus Theodorus <agustinustheoo@gmail.com>
2022-03-22 00:53:59 +0700

    feat: update hook script

--- .maintain/install-hooks.sh
+++ .maintain/install-hooks.sh
@@ -4,9 +4,11 @@ echo "#!/bin/sh
 make clean
 git add .
 " > .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
 
 touch .git/hooks/pre-push
 echo "#!/bin/sh
 
 make prepush
 " > .git/hooks/pre-push
+chmod +x .git/hooks/pre-push
