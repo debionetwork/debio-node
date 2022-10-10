f4c9660ce8e8f6ae1dfdd54d55d28ab6d0dd98c9
Irman Nur Muhammad Alamsyah <irmannmal@gmail.com>
2022-10-10 20:41:52 +0700

    ci: improments

--- .maintain/fix-lint-and-format.sh
+++ .maintain/fix-lint-and-format.sh
@@ -9,7 +9,7 @@ PROJECT_ROOT=`git rev-parse --show-toplevel`
 cd $PROJECT_ROOT
 
 echo "Fix Lint"
-cargo +nightly clippy --all --fix --allow-dirty
+cargo +nightly clippy --workspace --fix
 echo "Fix Format"
 cargo +nightly fmt --all
 
--- .maintain/test.sh
+++ .maintain/test.sh
@@ -8,29 +8,11 @@ pushd .
 PROJECT_ROOT=`git rev-parse --show-toplevel`
 cd $PROJECT_ROOT
 
-echo "Check Code"
-cargo +nightly check --all
-echo "Start Testing Code"
-for entry in `ls pallets`; do
-  pushd .
-
-  # The following line ensure we run from the project root
-  PROJECT_ROOT=`git rev-parse --show-toplevel`
-  cd $PROJECT_ROOT
-  cd "pallets/${entry}"
-
-  if [ -d "tests" ]; then
-    # Take action if "tests" exists. #
-    cd "tests"
-  fi
-
-  cargo +nightly test
-
-  popd
-done
 echo "Check Lint"
-cargo +nightly clippy --all -- -D warnings
+cargo +nightly clippy --workspace -- -D warnings
 echo "Check Format"
-cargo +nightly fmt --all -- --check
+cargo +nightly fmt --all --check
+echo "Start Testing"
+cargo +nightly test --workspace
 
 popd
